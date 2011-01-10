7a357d65cd41f74573975e38b9afd78b09c096d1
Joe Ricioppo <spuufmail@mac.com>
2011-01-10 01:26:42 -0800

    Tweak

--- MyWindowController.m
+++ MyWindowController.m
@@ -28,14 +28,14 @@
 	self.navBar.bottomEdgeColor = [NSColor colorWithCalibratedWhite:0.25 alpha:1.0];
 	
 
-	self.background.backgroundColor = [NSColor colorWithCalibratedHue:0.625 saturation:0.0 brightness:0.8 alpha:1.0];
-	self.background.innerShadow = [NSShadow shadowWithColor:[NSColor colorWithCalibratedHue:0.625 saturation:0.0 brightness:0.0 alpha:0.23]
-													 offset:NSMakeSize(0.0, 0.0)
-												 blurRadius:2.0];
+	self.background.backgroundColor = [NSColor colorWithCalibratedHue:0.625 saturation:0.0 brightness:0.82 alpha:1.0];
+//	self.background.innerShadow = [NSShadow shadowWithColor:[NSColor colorWithCalibratedHue:0.625 saturation:0.0 brightness:0.0 alpha:0.23]
+//													 offset:NSMakeSize(0.0, 0.0)
+//												 blurRadius:2.0];
 	self.background.innerGlow = [NSShadow shadowWithColor:[NSColor colorWithCalibratedHue:0.625 saturation:0.0 brightness:0.0 alpha:0.2]
 													 offset:NSMakeSize(0.0, 0.0)
 												 blurRadius:10.0];
-	self.background.bottomEdgeColor = [NSColor colorWithCalibratedWhite:0.25 alpha:1.0];
+	self.background.bottomEdgeColor = [NSColor colorWithCalibratedWhite:0.27 alpha:1.0];
 	
 	
 	self.toolBar.gradient = [NSGradient gradientWithStartingColor:[NSColor colorWithCalibratedHue:0.625 saturation:0.0 brightness:0.7 alpha:1.0] 
